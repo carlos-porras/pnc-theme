d3244d42450e24bf8a40133bfb5846f483926652
Carlos Porras <boxtricks.apps@gmail.com>
2016-06-11 23:17:13 -0500

    Creta popovers for search and login

--- js/bootstrap.config.js
+++ js/bootstrap.config.js
@@ -1,2 +1,9 @@
-$('[data-toggle="tooltip"]').tooltip();   
+$('[data-toggle="tooltip"]').tooltip();
+$('[data-toggle="popover"]').popover(); 
+
+var search_tpl = $('#search-tpl').html();
+$('#search-content').attr('data-content', search_tpl);
+
+var login_tpl = $('#login-tpl').html();
+$('#login-content').attr('data-content', login_tpl);
 
